e3e3ce2e52009cf66ff1e57bfa79fbec42dacf18
Juanma <juanma.soruco@gmail.com>
2026-04-18 09:17:07 -0400

    feat: tarea

--- app/mi-app/src/App.tsx
+++ app/mi-app/src/App.tsx
@@ -1,121 +1,111 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { useState, type JSX } from "react"; // hook de estado
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App(): JSX.Element {
+
+  const [numerosPares, setNumerosPares] = useState<number[]>([]);
+  const [numerosImpares, setNumerosImpares] = useState<number[]>([]);
+
+  const [nuevoNumero, setNumero] = useState<number>(0);
+
+  // Función para agregar producto
+  function agregarNumero(): void {
+
+    // Validar vacío
+    if (nuevoNumero === 0) return;
+    if (nuevoNumero <0 || nuevoNumero>100) {
+      alert("Por favor, ingrese un número entre 0 y 100");
+      return;
+    }
+
+    if(nuevoNumero % 2 === 0) {
+      if (numerosPares.includes(nuevoNumero)) {
+        alert("El numero par ya existe");
+        return; 
+      }
+      setNumerosPares([...numerosPares, nuevoNumero]);
+    }
+    else{
+      if (numerosImpares.includes(nuevoNumero)) {
+        alert("El numero impar ya existe");
+        return; 
+      }
+      setNumerosImpares([...numerosImpares, nuevoNumero]);
+    }
+    setNumero(0);
+  }
+
+  function eliminarNumeroImpar(indexEliminar: number): void {
+    const nuevaLista = [
+      ...numerosImpares.slice(0, indexEliminar),
+      ...numerosImpares.slice(indexEliminar + 1)
+    ];
+    setNumerosImpares(nuevaLista);
+  }
+
+  function eliminarNumeroPar(indexEliminar: number): void {
+    const nuevaLista = [
+      ...numerosPares.slice(0, indexEliminar),
+      ...numerosPares.slice(indexEliminar + 1)
+    ];
+    setNumerosPares(nuevaLista);
+  }
+
+  function manejarCambio(e: React.ChangeEvent<HTMLInputElement>): void {
+    setNumero(Number(e.target.value));
+  }
 
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
+    <div>
+      <h2>Clasificador de numeros</h2>
+      <input
+        type="number"
+        placeholder="Ej: 3"
+        value={nuevoNumero}
+        onChange={manejarCambio}
+      />
 
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
+      <button onClick={agregarNumero}>
+        Agregar
+      </button>
+      <h2>Numeros Pares</h2>
+      {
+        numerosPares.length === 0
+        ? <p>No hay numeros pares</p>
+        : (
           <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
+            {
+              numerosPares.map((numero, index) => (
+                <li key={index}>
+                  {numero}
+                  <button onClick={() => eliminarNumeroPar(index)}>
+                    Eliminar
+                  </button>
+                </li>
+              ))
+            }
           </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
+        )
+      }
+      <h2>Numeros Impares</h2>
+      {
+        numerosImpares.length === 0
+        ? <p>No hay numeros impares</p>
+        : (
           <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
+            {
+              numerosImpares.map((numero, index) => (
+                <li key={index}>
+                  {numero}
+                  <button onClick={() => eliminarNumeroImpar(index)}>
+                    Eliminar
+                  </button>
+                </li>
+              ))
+            }
           </ul>
-        </div>
-      </section>
+        )
+      }
 
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
-  )
+    </div>
+  );
 }
-
-export default App
